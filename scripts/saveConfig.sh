0f960014f224bbe2ff3b3f34a658d59fbbc3b983
jrootham <jrootham@gmail.com>
2020-02-25 15:51:16 -0500

    Update saveConfig

--- scripts/saveConfig.sh
+++ scripts/saveConfig.sh
@@ -4,6 +4,8 @@
 
 cp /etc/nginx/sites-available/* /home/jrootham/dev/website/config/local/nginx
 cp /etc/supervisor/supervisord.conf /home/jrootham/dev/website/config/local/supervisor
+cp -r /etc/supervisor/conf.d /home/jrootham/dev/website/config/local/supervisor
 
 scp jrootham@jrootham.ca:/etc/nginx/sites-available/\* /home/jrootham/dev/website/config/remote/nginx
 scp jrootham@jrootham.ca:/etc/supervisor/supervisord.conf /home/jrootham/dev/website/config/remote/supervisor
+scp -r jrootham@jrootham.ca:/etc/supervisor/conf.d /home/jrootham/dev/website/config/remote/supervisor
